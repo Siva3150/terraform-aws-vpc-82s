bacb0571e0529c2015149d8a64f804873e473228
Siva prasad <sivaprasad3150@gmail.com>
2025-02-14 03:22:50 +0530

    user-profile

---
+++ variables.tf
@@ -0,0 +1,72 @@
+variable "cidr_block" {
+    #default = 10.0.0.0/16
+}
+
+variable "common_tags" {
+    default = {}
+}
+
+variable "vpc_tags" {
+    default = {}
+}
+
+variable "environment" {
+    type = string
+}
+
+variable "project_name" {
+    type = string
+}
+
+variable "igw_tags" {
+    default = {}
+}
+
+variable "public_subnet_tags" {
+    default = {}
+}
+
+variable "public_subnet_cidrs" {
+    type = list 
+    validation {
+      condition = length(var.public_subnet_cidrs) == 2
+      error_message = "please provide 2 valid public subnet cidrs"
+    }
+}
+
+variable "private_subnet_cidrs" {
+    type = list 
+    validation {
+      condition = length(var.private_subnet_cidrs) == 2
+      error_message = "please provide 2 valid public subnet cidrs"
+    }
+}
+
+variable "private_subnet_tags" {
+    default = {}
+}
+
+variable "database_subnet_cidrs" {
+    type = list 
+    validation {
+      condition = length(var.database_subnet_cidrs) == 2
+      error_message = "please provide 2 valid public subnet cidrs"
+    }
+}
+
+variable "database_subnet_tags" {
+    default = {}
+}
+
+variable "db_subnet_group_tags" {
+    default = {}
+} 
+
+variable "nat_gateway_tags" {
+    default = {}
+}
+
+
+
+
+
